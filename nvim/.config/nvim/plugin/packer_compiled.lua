705f8e75d24184b5816c803301481839b8940f7e
kitkovsky <okitkowski114@gmail.com>
2022-08-11 17:45:30 +0200

    remove unused colorizer plugin

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -149,11 +149,6 @@ _G.packer_plugins = {
     path = "/home/kitkovsky/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
-  ["nvim-colorizer.lua"] = {
-    loaded = true,
-    path = "/home/kitkovsky/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
-    url = "https://github.com/norcalli/nvim-colorizer.lua"
-  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/home/kitkovsky/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
